66b5b222a1b82aee9c381422145b09047123eca8
purerosefallen <78877@qq.com>
2018-10-29 18:36:25 +0800

    first

---
+++ script/special.lua
@@ -0,0 +1,107 @@
+DOOM_LEVEL=0
+function Auxiliary.DoomChance(p)
+	return math.random()<=DOOM_LEVEL*0.1*p
+end
+function Auxiliary.DoomValue(p)
+	return math.random(math.ceil(DOOM_LEVEL*0.1*p))
+end
+function Auxiliary.PreloadUds()
+	local e1=Effect.GlobalEffect()
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_ADJUST)
+	e1:SetOperation(function(e)
+		DOOM_LEVEL=Duel.AnnounceNumber(1,0,1,2,3,4,5,6,7,8,9,10)
+		if DOOM_LEVEL>0 then
+			local e1=Effect.GlobalEffect()
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetTargetRange(1,0)
+			e1:SetCode(EFFECT_DRAW_COUNT)
+			e1:SetValue(function(e)
+				return 1+Auxiliary.DoomValue(5)
+			end)
+			Duel.RegisterEffect(e1,0)
+			local e1=Effect.GlobalEffect()
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetTargetRange(1,0)
+			e1:SetCode(EFFECT_HAND_LIMIT)
+			e1:SetValue(100)
+			Duel.RegisterEffect(e1,0)
+			local e1=Effect.GlobalEffect()
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PHASE_START+PHASE_DRAW)
+			e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)
+				return Duel.GetTurnPlayer()==0
+			end)
+			e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+				if Duel.GetFieldGroupCount(0,LOCATION_DECK,0)==0 then
+					local g=Duel.GetFieldGroup(0,LOCATION_GRAVE+LOCATION_REMOVED,0)
+					Duel.SendtoDeck(g,nil,2,REASON_RULE)
+					Duel.ShuffleDeck(0)
+				end
+				Duel.Recover(0,Auxiliary.DoomValue(2000),REASON_RULE)
+				local e1=Effect.GlobalEffect()
+				e1:SetType(EFFECT_TYPE_FIELD)
+				e1:SetCode(EFFECT_SET_SUMMON_COUNT_LIMIT)
+				e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+				e1:SetTargetRange(1,0)
+				e1:SetValue(1+Auxiliary.DoomValue(10))
+				e1:SetReset(RESET_PHASE+PHASE_END)
+				Duel.RegisterEffect(e1,0)
+			end)
+			Duel.RegisterEffect(e1,0)
+			if Duel.IsExistingMatchingCard(Card.IsCode,0,LOCATION_EXTRA,0,1,nil,48905153) then
+				local e1=Effect.GlobalEffect()
+				e1:SetType(EFFECT_TYPE_FIELD)
+				e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+				e1:SetTargetRange(1,0)
+				e1:SetCode(EFFECT_EXTRA_TOMAIN_KOISHI)
+				e1:SetValue(1)
+				Duel.RegisterEffect(e1,0)
+			end
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+			e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+			e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)
+				local tc=Duel.GetAttacker()
+				return tc and tc:IsControler(0)
+			end)
+			e3:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+				local tc=Duel.GetAttacker()
+				if not tc then return end
+				local atk=tc:GetAttack()
+				local e1=Effect.CreateEffect(tc)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_UPDATE_ATTACK)
+				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+				e1:SetValue(Auxiliary.DoomValue(atk))
+				tc:RegisterEffect(e1,true)
+			end)
+			Duel.RegisterEffect(e1,0)
+			local reg=Card.RegisterEffect
+			Card.RegisterEffect=function(c,e,b)
+				if e:IsHasType(0x7f0) then
+					local con=e:GetCondition()
+					e:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)
+						return not con or con(e,tp,eg,ep,ev,re,r,rp) or Auxiliary.DoomChance(0.5)
+					end)
+					local cost=e:GetCost()
+					e:SetCost(function(e,tp,eg,ep,ev,re,r,rp,chk)
+						if chk==0 then return not cost or cost(e,tp,eg,ep,ev,re,r,rp,0) or Auxiliary.DoomChance(0.5) end
+						if cost and cost(e,tp,eg,ep,ev,re,r,rp,0) and not Auxiliary.DoomChance(0.5) then
+							cost(e,tp,eg,ep,ev,re,r,rp,1)
+						end
+					end)
+				end
+				reg(c,e,b)
+			end
+			local lp=Duel.GetLP(0)
+			Duel.SetLP(0,lp+Auxiliary.DoomValue(lp))
+		end
+		e:Reset()
+	end)
+	Duel.RegisterEffect(e1,1)
+end
