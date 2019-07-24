ba98fbb9a83eb4e023f9e3ac3c14c328321d180e
purerosefallen <78877@qq.com>
2019-07-24 13:57:29 +0800

    add nodejs plguin

---
+++ index.js
@@ -0,0 +1,32 @@
+
+ygopro.ctos_follow_before('HS_TOOBSERVER', true, (buffer, info, client, server, datas) => {
+	return true;
+});
+
+ygopro.ctos_follow_before('HS_KICK', true, (buffer, info, client, server, datas) => {
+	return true;
+});
+
+ygopro.stoc_follow_before('HAND_RESULT', true, (buffer, info, client, server, datas) => {
+	return true;
+});
+
+ygopro.stoc_follow_before('SELECT_HAND', true, (buffer, info, client, server, datas) => {
+	var room = ROOM_all[client.rid];
+	if (!room) {
+		return false;
+	}
+	room.duel_stage = ygopro.constants.DUEL_STAGE.FINGER;
+	ygopro.ctos_send(server, "HAND_RESULT", {
+		res: client.is_local ? 2 : 1
+	});
+	return true;
+});
+
+ygopro.ctos_follow_before("JOIN_GAME", false, (buffer, info, client, server, datas) => {
+	var struct = ygopro.structs["deck"];
+	struct._setBuff(CTOS_JoinGame);
+	struct.set("pass", "AI");
+	buffer = struct.buffer;
+	return false;
+});
