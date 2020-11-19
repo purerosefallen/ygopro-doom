dc6aa3655284770409f987e83ace2ea79899c1d4
purerosefallen <78877@qq.com>
2020-11-19 08:36:31 +0800

    update doom

--- index.js
+++ index.js
@@ -11,23 +11,20 @@ ygopro.stoc_follow_before('HAND_RESULT', true, async (buffer, info, client, serv
 	return true;
 });
 
-ygopro.stoc_follow_after('SELECT_HAND', true, async (buffer, info, client, server, datas) => {
-	var room = ROOM_all[client.rid];
-	if (!room) {
+ygopro.stoc_follow_before('JOIN_GAME', true, async (buffer, info, client, server, datas) => {
+	const room = ROOM_all[client.rid];
+	if (!room || !client.is_local) {
 		return false;
 	}
-	room.duel_stage = ygopro.constants.DUEL_STAGE.FINGER;
-	ygopro.ctos_send(server, "HAND_RESULT", {
-		res: client.is_local ? 2 : 1
-	});
-	return true;
+	room.determine_firstgo = client;
+	return false;
 });
 
 ygopro.ctos_follow_before("JOIN_GAME", false, async (buffer, info, client, server, datas) => {
 	if (client.is_local) {
 		return false;
 	}
-	var struct = ygopro.structs.get("CTOS_JoinGame");
+	const struct = ygopro.structs.get("CTOS_JoinGame");
 	struct._setBuff(buffer);
 	struct.set("pass", "AI");
 	buffer = struct.buffer;
